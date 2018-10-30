319d123aa93cd2e25b9c8276db04a40c144152f8
Apple <resume_cool@hotmail.com>
2018-10-30 16:14:58 +0800

    Initial Commit

---
+++ 20181030/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  20181030
+//
+//  Created by Apple on 2018/10/30.
+//  Copyright © 2018 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
